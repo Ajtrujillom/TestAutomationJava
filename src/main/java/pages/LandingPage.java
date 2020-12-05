3abba01078fb375091dfe70533946aa695cb378c
Ajtrujillomn <jorge.pliego@wizeline.com>
2020-12-04 19:34:06 -0600

    adding base page implementation

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -1,12 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LandingPage {
+public class LandingPage extends  BasePage{
 
     @FindBy(css = ".product_label")
     private WebElement header;
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+    }
 
     public boolean isHeaderVisible(){
         return header.isDisplayed();
--- src/main/java/pages/ShoppingCartPage.java
+++ src/main/java/pages/ShoppingCartPage.java
@@ -1,14 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ShoppingCartPage {
+public class ShoppingCartPage extends BasePage {
     @FindBy(css="a.btn_action")
     private WebElement checkoutBtn;
 
     @FindBy(css="a.btn_secondary")
     private  WebElement continueShoppingBtn;
+
+    public ShoppingCartPage(WebDriver driver) {
+        super(driver);
+    }
 
 
     public void clickCheckoutBtn(){
