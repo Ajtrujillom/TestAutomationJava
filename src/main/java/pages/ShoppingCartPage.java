7e19ef19334911dfcc21816a44d6adc400e22d67
Ajtrujillomn <jorge.pliego@wizeline.com>
2020-12-04 19:15:47 -0600

    mapping the other button

--- src/main/java/pages/ShoppingCartPage.java
+++ src/main/java/pages/ShoppingCartPage.java
@@ -7,7 +7,15 @@ public class ShoppingCartPage {
     @FindBy(css="a.btn_action")
     private WebElement checkoutBtn;
 
+    @FindBy(css="a.btn_secondary")
+    private  WebElement continueShoppingBtn;
+
+
     public void clickCheckoutBtn(){
         this.checkoutBtn.click();
+    }
+
+    public void clickContinueShoppingBtn(){
+        this.continueShoppingBtn.click();
     }
 }
